d166522e0a0a75153f02ebbcf99fe63aef10bbb1
Tron <trononly1@gmail.com>
2025-06-12 20:48:23 +0300

    config fix

--- next.config.ts
+++ next.config.ts
@@ -24,13 +24,7 @@ const nextConfig: NextConfig = withPWA({
         pathname: '/**',
       },
     ],
-  },
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-  },
-};
+  }
+});
 
 export default nextConfig;
